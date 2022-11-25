146a392c32a85fb9ffe689976776dbe24dfde3a5
TSANT17 <89149217+TSANT17@users.noreply.github.com>
2022-11-25 14:58:11 -0500

    Add files via upload

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
-import Routes from "../../Routers";
+import Routes from "../../routes/Routers";
 
 import Carts from "../UI/cart/Carts.jsx";
 import { useSelector } from "react-redux";
